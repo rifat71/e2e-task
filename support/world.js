5130dd8dd1dfd474246d9ee6ec7a5150bfebb925
rifat71 <rifatmahmud24311@gmail.com>
2025-03-17 01:49:02 +0600

    add headless

--- support/world.js
+++ support/world.js
@@ -1,15 +1,24 @@
 const { setWorldConstructor } = require('@cucumber/cucumber');
-const Promise = require(`bluebird`);
+const Promise = require('bluebird');
 const Screenshot = require('../utils/Screenshot');
+const { chromium } = require('playwright');
 
 class World {
-
   constructor({ attach }) {
     this.attach = attach;
     this.screenshot = new Screenshot(this);
     this.browser = null;
     this.context = null;
     this.page = null;
+  }
+
+  async launchBrowser() {
+    this.browser = await chromium.launch({
+      headless: process.env.HEADLESS == 'false',
+    });
+
+    this.context = await this.browser.newContext();
+    this.page = await this.context.newPage();
   }
 
   sleep(milliseconds) {
