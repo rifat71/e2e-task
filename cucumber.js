c1490010a17a9121d42517cbaf692c9be49b6234
rifat71 <rifatmahmud24311@gmail.com>
2025-03-16 20:32:06 +0600

    feature split

--- cucumber.js
+++ cucumber.js
@@ -2,8 +2,8 @@ module.exports = {
   default: {
     format: ['progress', 'json:reports/cucumber_report.json'],
     require: [
-      'support/hooks.js',  // Ensure hooks are loaded
-      'support/world.js',  // Ensure World is loaded
+      'support/hooks.js',
+      'support/world.js',
       'features/step_definitions/**/*.js',
     ],
     tags: 'not @ignore',
